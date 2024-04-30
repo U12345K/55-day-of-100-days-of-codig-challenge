80a7452bab3fac42290245ca1d8d7af8e7a1e102
umarkhan <ku6205939@gmail.com>
2024-04-30 15:40:59 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+//task 1
+Object.defineProperty(exports, "__esModule", { value: true });
+let neme = "Ilya";
+console.log(`hello ${1}`);
+console.log(`hello ${"name"}`);
+console.log(`hello ${neme}`);
+//task2
+let yourname = prompt("what is your name?", "");
+console.log(yourname);
---
+++ main.ts
@@ -0,0 +1,11 @@
+//task 1
+
+let neme="Ilya";
+console.log(`hello ${1}`);
+console.log(`hello ${"name"}`);
+console.log(`hello ${neme}`);
+
+//task2
+
+let yourname=prompt("what is your name?","");
+console.log(yourname);
